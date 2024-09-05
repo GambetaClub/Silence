948a7b48113f8f3b1d5a5e8cd8ddd081032b3708
GambetaClub <marigyt.maj@gmail.com>
2024-09-05 15:20:48 -0500

    "Removing the generateStaticPage because for some reason the database is not reachable when building"

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,19 +2,11 @@ import { fetchArticleById } from "@/actions/server/articles"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import React from "react"
 import ArticleEditForm from "./components/ArticleEditForm"
-import db from "@/lib/database"
 
 interface ArticlePageProps {
   params: { id: string }
 }
 
-export async function generateStaticParams() {
-  const articles = await db.article.findMany({ take: 40 })
-
-  return articles.map((article) => ({
-    id: article.id,
-  }))
-}
 
 const ArticlePage = async ({ params }: ArticlePageProps) => {
   const article = await fetchArticleById(params.id)
